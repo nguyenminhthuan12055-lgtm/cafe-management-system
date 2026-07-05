764900fd324889fdf2123e82d7f779723c0f0d5e
nguyenminhthuan12055-lgtm <nguyenminhthuan12055@gmail.com>
2026-07-05 11:24:05 +0700

    Delete backend directory

--- backend/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
-
-router.post("/api/auth/login", userController.login);
-router.post("/api/auth/register", userController.register);
-
-module.exports = router;
